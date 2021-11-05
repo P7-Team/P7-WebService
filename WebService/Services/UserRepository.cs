d0c4f7ad32213f6e7635189649e1cf8b8b4644e9
soenderby <jkr18@student.aau.dk>
2021-11-05 13:29:19 +0100

    Created IRepository and IAggregateRoot interfaces
    Also created an implementation of IRepository in the form of UserRepository

---
+++ WebService/Services/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Interfaces;
+using WebService.Models;
+
+namespace WebService.Services
+{
+    public class UserRepository : IRepository<User, int>
+    {
+        public void Create(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Read(int identifer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
