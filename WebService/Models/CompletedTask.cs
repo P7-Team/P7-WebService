1f5c0a803f1cc0ba87287fb5b1b8588daec419e7
Alex <alex.klitgaard@gmail.com>
2021-11-15 10:52:00 +0100

    Added CompleteTask receiving to the TaskController
    Added functionality for receiving a CompletedTask to the TaskController and added constructors to CompletedTask so that it can be instantiated.

--- WebService/Models/CompletedTask.cs
+++ WebService/Models/CompletedTask.cs
@@ -2,8 +2,19 @@
 {
     public class CompletedTask
     {
-        public long TaskId { get; set; }
+        public long TaskId { get; protected set; }
         
-        public string Result { get; set; }
+        public string Result { get; protected set; }
+
+        public CompletedTask(long taskId, string result)
+        {
+            TaskId = taskId;
+            Result = result;
+        }
+
+        public CompletedTask()
+        {
+            // For deserialization
+        }
     }
 }
