30535dd9d63c4f7cb324d010a65493ad155bdefc
Alexander N <alax0013@gmail.com>
2021-11-17 13:21:57 +0100

    Timer now works.

--- WebService/Services/Scheduler.cs
+++ WebService/Services/Scheduler.cs
@@ -1,31 +1,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using WebService.Interfaces;
 using WebService.Models;
 
 namespace WebService.Services
 {
-    class TaskWrapper
+    public class TaskWrapper
     {
-        public int TaskNumber;
-        public long Id;
         public DateTime LastPing;
-        public string Assignee;
         public bool isDone;
+        public User user;
+        public Task Task;
 
-        public TaskWrapper(Task task)
+        public TaskWrapper(Task task, User user)
         {
-            TaskNumber = task.Number;
-            Id = task.Id;
+            this.user = user;
             isDone = false;
+            Task = task;
             LastPing = DateTime.Now;
-            Assignee = task.AllocatedTo;
         }
     }
 
     public class SchedulerController : IScheduler
     {
+        private static Timer _aTimer;
+
         // TODO Implement functionality such that the user can only be assigned to a specific task once.
         private List<Batch> _batches;
 
@@ -35,6 +36,7 @@ namespace WebService.Services
         {
             _assignedUsers = new List<TaskWrapper>();
             _batches = new List<Batch>();
+            SetTimer();
         }
 
         public Task GetTaskAndAssignUser(User user)
@@ -47,11 +49,11 @@ namespace WebService.Services
 
                     if (tempTask.AllocatedTo != null) continue;
 
-                    if (_assignedUsers.Any(x => x.Assignee == user.Username
-                                                && x.Id == currentBatch.Id
-                                                && x.TaskNumber == tempTask.Number)) continue;
+                    if (_assignedUsers.Any(x => x.Task.AllocatedTo == user.Username
+                                                && x.Task.Id == currentBatch.Id
+                                                && x.Task.Number == tempTask.Number)) continue;
                     tempTask.SetAllocatedTo(user);
-                    TaskWrapper tw = new TaskWrapper(tempTask);
+                    TaskWrapper tw = new TaskWrapper(tempTask, user);
                     _assignedUsers.Add(tw);
                     return tempTask;
                 }
@@ -79,7 +81,7 @@ namespace WebService.Services
                 if (batch.TasksCount() == 0) batchesToRemove.Add(batch);
             }
 
-            foreach (var currentTask in _assignedUsers.Where(x => x.Id == id && x.TaskNumber == number))
+            foreach (var currentTask in _assignedUsers.Where(x => x.Task.Id == id && x.Task.Number == number))
             {
                 currentTask.isDone = true;
             }
@@ -101,7 +103,7 @@ namespace WebService.Services
         public void PingScheduler(User user, DateTime dateTime)
         {
             // TODO Implement timestamp on a task for when a user last pinged the server.
-            foreach (TaskWrapper taskWrapper in _assignedUsers.Where(x => x.Assignee == user.Username && !x.isDone))
+            foreach (TaskWrapper taskWrapper in _assignedUsers.Where(x => x.Task.AllocatedTo == user.Username && !x.isDone))
             {
                 taskWrapper.LastPing = dateTime;
             }
@@ -109,12 +111,32 @@ namespace WebService.Services
 
         public DateTime? GetLastPing(User user)
         {
-            foreach (TaskWrapper taskWrapper in _assignedUsers.Where(x => x.Assignee == user.Username && !x.isDone))
+            foreach (TaskWrapper taskWrapper in _assignedUsers.Where(x => x.Task.AllocatedTo == user.Username && !x.isDone))
             {
                 return taskWrapper.LastPing;
             }
 
             return null;
+        }
+
+        private void SetTimer()
+        {
+            // Create a timer with a 2 min interval.
+            _aTimer = new Timer(1000*60*2);
+            // Hook up the Elapsed event for the timer. 
+            _aTimer.Elapsed += (s, e) => UserStillWorking();
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
+        }
+
+        private void UserStillWorking()
+        {
+            DateTime currentTime = DateTime.Now;
+            foreach (var tw in _assignedUsers.Where(x =>
+                !x.isDone && x.LastPing < currentTime.Subtract(new TimeSpan(0, 5, 0))))
+            {
+                UnAssignUserFromTask(tw.user, tw.Task.Id, tw.Task.Number, tw.Task.SubNumber);
+            }
         }
     }
 }
