ac8e6bca356bf37edb803c71dde6eb350cfad02e
soenderby <jkr18@student.aau.dk>
2021-11-17 13:12:41 +0100

    Created InputFile model and repository

---
+++ WebService/Models/InputFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Interfaces;
+using WebService.Services;
+
+namespace WebService.Models
+{
+    public class InputFile : IAggregateRoot<(string path, string filename)>
+    {
+        public string Path { get; set; }
+
+        public string Filename { get; set; }
+
+        public string Encoding { get; set; }
+
+        public int BatchId { get; set; }
+
+        public (string path, string filename) GetIdentifier()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebService/Services/Repositories/InputFileRepository.cs
@@ -0,0 +1,59 @@
+﻿using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebService.Interfaces;
+using WebService.Models;
+using WebService.Services;
+
+namespace WebService.Services.Repositories
+{
+    public class InputFileRepository : IRepository<InputFile, (string path, string filename)>
+    {
+        private readonly QueryFactory _db;
+        private const string table = "File";
+
+        public InputFileRepository(QueryFactory db)
+        {
+            _db = db;
+        }
+
+        public void Create(InputFile item)
+        {
+            _db.Query(table).Insert(new
+            {
+                path = item.Path,
+                filename = item.Filename,
+                encoding = item.Encoding,
+                includedIn = item.BatchId
+            });
+        }
+
+        public InputFile Read((string path, string filename) identifier)
+        {
+            return _db.Query(table).Where(new
+            {
+                path = identifier.path,
+                filename = identifier.filename
+            }).First();
+        }
+
+        public void Update(InputFile item)
+        {
+            _db.Query(table).Where(new
+            {
+                path = item.Path,
+                filename = item.Filename
+            }).Update(new { encoding = item.Encoding, includedIn = item.BatchId });
+        }
+
+        public void Delete((string path, string filename) identifier)
+        {
+            _db.Query(table).Where(new
+            {
+                path = identifier.path,
+                filename = identifier.filename
+            }).Delete();
+        }
+    }
+}
