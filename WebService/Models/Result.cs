c064b8312b940ada0fefcd6352e2792da226c194
soenderby <jkr18@student.aau.dk>
2021-11-18 11:10:29 +0100

    Created Result and ResultRepository

---
+++ WebService/Models/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Interfaces;
+
+namespace WebService.Models
+{
+    public class Result : IAggregateRoot<(string path, string filename)>
+    {
+        public string Path { get; set; }
+        public string Filename { get; set; }
+        public bool Verfied { get; set; }
+        public Task Task { get; set; }
+
+        public Result(string path, string filename, bool verified, Task task)
+        {
+            Path = path;
+            Filename = filename;
+            Verfied = verified;
+            Task = task;
+        }
+
+        public (string path, string filename) GetIdentifier()
+        {
+            return (Path, Filename);
+        }
+    }
+}
---
+++ WebService/Services/Repositories/ResultRepository.cs
@@ -0,0 +1,73 @@
+﻿using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Interfaces;
+using WebService.Models;
+
+namespace WebService.Services.Repositories
+{
+    public class ResultRepository : IRepository<Result, (string path, string filename)>
+    {
+        private readonly QueryFactory _db;
+        private const string table = "Result";
+
+        public ResultRepository(QueryFactory db)
+        {
+            _db = db;
+        }
+
+        public void Create(Result item)
+        {
+            _db.Query(table).Insert(new
+            {
+                path = item.Path,
+                filename = item.Filename,
+                isVerified = item.Verfied,
+                task_id = item.Task.Id,
+                task_number = item.Task.Number,
+                task_subnumber = item.Task.SubNumber
+            });
+        }
+
+        public Result Read((string path, string filename) identifier)
+        {
+            return _db.Query(table).Where(new
+            {
+                path = identifier.path,
+                filename = identifier.filename
+            })
+                .Join("Task", j =>
+                    j.On("Result.task_id", "Task.id")
+                    .On("Result.task_number", "Task.number")
+                    .On("Result.task_subnumber", "Task.subNumber")
+                 )
+                .First<Result>();
+        }
+
+        public void Update(Result item)
+        {
+            _db.Query(table).Where(new
+            {
+                path = item.Path,
+                filename = item.Filename
+            }).Update(new
+            {
+                isVerified = item.Verfied,
+                task_id = item.Task.Id,
+                task_number = item.Task.Number,
+                task_subnumber = item.Task.SubNumber
+            });
+        }
+
+        public void Delete((string path, string filename) identifier)
+        {
+            _db.Query(table).Where(new
+            {
+                path = identifier.path,
+                filename = identifier.filename,
+            }).Delete();
+        }
+    }
+}
