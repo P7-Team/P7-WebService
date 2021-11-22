fbf84e94f6de674b9f3dcbed7d0682e9d34783a4
Alex <alex.klitgaard@gmail.com>
2021-11-22 13:06:11 +0100

    Added model of a BatchStatus

---
+++ WebService/Models/BatchStatus.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop.Infrastructure;
+
+namespace WebService.Models
+{
+    public class BatchStatus
+    {
+        private bool Finished { get; set; }
+        private int Total { get; set; }
+        private int Done { get; set; }
+        private int Id { get; set; }
+
+        public BatchStatus(int id, bool finished, int done, int total)
+        {
+            Id = id;
+            Finished = finished;
+            Done = done;
+            Total = total;
+        }
+    }
+}
