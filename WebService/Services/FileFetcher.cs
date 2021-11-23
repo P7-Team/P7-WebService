7d1463b3ec8136d530d608d824a6b11c061342cc
Alex <alex.klitgaard@gmail.com>
2021-11-23 09:54:22 +0100

    Implemeted and documented a FileFetcher

---
+++ WebService/Services/FileFetcher.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using WebService.Interfaces;
+
+namespace WebService.Services
+{
+    public class FileFetcher : IFileFetcher
+    {
+        public Stream Fetch(string path, string filename, string extension)
+        {
+            Stream fileStream = File.OpenRead(path + filename + extension);
+            return fileStream;
+        }
+    }
+}
