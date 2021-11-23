a4a59f5d4df35f37adc0d46d6e7f2798c42051aa
soenderby <jkr18@student.aau.dk>
2021-11-23 11:02:30 +0100

    Created store folder

---
+++ WebService-UnitTests/BatchStoreTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using WebService.Interfaces;
+using WebService.Models;
+using WebService.Services;
+using WebService.Services.Stores;
+using WebService.Services.Repositories;
+
+namespace WebService_UnitTests
+{
+    class MockBatchRepository : IRepository<Batch, int>
+    {
+        public Batch CalledWithBatch { get; private set; }
+
+        public void Create(Batch item)
+        {
+            CalledWithBatch = item;
+        }
+
+        public void Delete(int identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Batch Read(int identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Batch item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BatchStoreTests
+    {
+        [Fact]
+        public void Store_GivenBatch_SavesBatchUsingGivenRepository()
+        {
+            Batch testBatch = new Batch(182);
+            MockBatchRepository rep = new MockBatchRepository();
+            BatchStore store = new BatchStore(rep);
+            store.Store(testBatch);
+
+            Assert.Equal(testBatch, rep.CalledWithBatch);
+        }
+
+        [Fact]
+        public void Store_Given()
+        {
+
+        }
+    }
+}
---
+++ WebService/Services/Stores/BatchStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Models;
+using WebService.Services;
+using WebService.Interfaces;
+
+namespace WebService.Services.Stores
+{
+    public class BatchStore
+    {
+        private readonly IRepository<Batch, int> _batchRepository;
+
+        public BatchStore(IRepository<Batch, int> batchRepository)
+        {
+            _batchRepository = batchRepository;
+        }
+
+        public void Store(Batch batch)
+        {
+            _batchRepository.Create(batch);
+        }
+    }
+}
