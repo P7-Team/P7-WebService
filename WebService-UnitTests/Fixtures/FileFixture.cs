9506464e9e1101921e87d1e1d96a68613888d3c4
Alex <alex.klitgaard@gmail.com>
2021-11-23 10:31:02 +0100

    Extracted FileFixture to its own file

---
+++ WebService-UnitTests/Fixtures/FileFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WebService_UnitTests.Fixtures
+{
+    public class FileFixture : IDisposable
+    {
+        public List<string> FilesToDelete { get; private set; }
+
+        public FileFixture()
+        {
+            FilesToDelete = new List<string>();
+        }
+
+        public void Dispose()
+        {
+            foreach (string file in FilesToDelete)
+            {
+                if (File.Exists(file))
+                    File.Delete(file);
+            }
+        }
+    }
+}
