2ae58d984d9875a9caecc579bdbac932baec4d8b
soenderby <jkr18@student.aau.dk>
2021-11-25 10:42:04 +0100

    Finished testing BatchRepository

--- IntegrationTests/BatchRepositoryTests.cs
+++ IntegrationTests/BatchRepositoryTests.cs
@@ -29,12 +29,14 @@ namespace IntegrationTests
                                 SET FOREIGN_KEY_CHECKS = 1; ");
 
             _userRepository = new UserRepository(Db);
-            _userRepository.Create(new WebService.User("batchTestUser", "test"));
+            _userRepository.Create(new WebService.User("batchTestUser1", "test"));
+            _userRepository.Create(new WebService.User("batchTestUser2", "test"));
         }
 
         public void Dispose()
         {
-            _userRepository.Delete("batchTestUser");
+            _userRepository.Delete("batchTestUser1");
+            _userRepository.Delete("batchTestUser2");
         }
 
         public QueryFactory Db { get; private set; }
@@ -44,8 +46,8 @@ namespace IntegrationTests
     {
         // change to skip = null, in order to run integration tests
         // Check https://josephwoodward.co.uk/2019/01/skipping-xunit-tests-based-on-runtime-conditions for conditional skip
-        //const string skip = "Integration Test, should not be run along with unit tests";
-        const string skip = null;
+        const string skip = "Integration Test, should not be run along with unit tests";
+        //const string skip = null;
         BatchRepository repository;
 
         public BatchRepositoryTests(BatchDatabaseFixture fixture)
@@ -56,12 +58,40 @@ namespace IntegrationTests
         [Fact(Skip = skip)]
         public void Repository_GivenBatch_InsertedBatchCanBeRead()
         {
-            Batch batch = new Batch() { OwnerUsername = "batchTestUser" };
+            Batch batch = new Batch() { OwnerUsername = "batchTestUser1" };
             int id = repository.Create(batch);
 
             Batch result = repository.Read(id);
 
-            Assert.Equal("batchTestUser", result.OwnerUsername);
+            Assert.Equal("batchTestUser1", result.OwnerUsername);
+        }
+
+        [Fact(Skip = skip)]
+        public void Update_GivenValidBatch_UpdatedBatchInDB()
+        {
+            Batch oldBatch = new Batch() { OwnerUsername = "batchTestUser1" };
+            int id = repository.Create(oldBatch);
+
+            Batch newBatch = new Batch() { Id = id, OwnerUsername = "batchTestUser2" };
+            repository.Update(newBatch);
+
+            Batch result = repository.Read(id);
+
+            Assert.Equal("batchTestUser2", result.OwnerUsername);
+        }
+
+        [Fact(Skip = skip)]
+        public void Delete_GivenIdForExistingBatch_RemovesBatchFromDB()
+        {
+            Batch batch = new Batch() { OwnerUsername = "batchTestUser1" };
+            // Insert user to be deleted, and ensure that it is in the DB
+            int id = repository.Create(batch);
+            Batch resultBeforeDelete = repository.Read(id);
+            Assert.Equal("batchTestUser1", resultBeforeDelete.OwnerUsername);
+
+            repository.Delete(id);
+
+            Assert.Throws<InvalidOperationException>(() => { repository.Read(id); });
         }
     }
 }
