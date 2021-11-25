eea1e9853845511ab60bde5ceb6ed2afd30b367b
Alex <alex.klitgaard@gmail.com>
2021-11-25 11:58:49 +0100

    Started implementing sourceFileRepository

--- WebService/Services/Repositories/SourceFileRepository.cs
+++ WebService/Services/Repositories/SourceFileRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SqlKata.Execution;
 using WebService.Interfaces;
 using WebService.Models;
 
@@ -9,14 +10,29 @@ namespace WebService.Services.Repositories
 {
     public class SourceFileRepository : IRepository<SourceFile, (string path, string filename)>
     {
+        private QueryFactory _db;
+        private const string _table = "Source";
+        public SourceFileRepository(QueryFactory db)
+        {
+            _db = db;
+        }
+        
         public (string path, string filename) Create(SourceFile item)
         {
-            throw new NotImplementedException();
+            _db.Query(_table).Insert(new
+            {
+                language = item.Language,
+            });
+            return (item.Path, item.Filename);
         }
 
         public void Delete((string path, string filename) identifier)
         {
-            throw new NotImplementedException();
+            _db.Query(_table).Where(new
+            {
+                path = identifier.path,
+                filename = identifier.filename,
+            }).Delete();
         }
 
         public SourceFile Read((string path, string filename) identifier)
