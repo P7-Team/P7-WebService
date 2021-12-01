428daf9b7ff078894314d53e2eacfbc6f4d8a72c
dkalaxdk <alax0013@gmail.com>
2021-12-01 11:45:19 +0100

    Timer can now periodically ask for a batch and add these to the scheduler. The specific asking for the batches is not defined yet.

--- WebService/Services/Automator.cs
+++ WebService/Services/Automator.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using WebService.Interfaces;
+using WebService.Models;
 using Timer = System.Timers.Timer;
 
 namespace WebService.Services
@@ -9,27 +11,32 @@ namespace WebService.Services
     {
         private ISchedulerWorkedOnHelper _schedulerWorkedOnHelper;
         private IContributionPointCalculator _cp;
-        private Thread _cleanInactiveUsers;
-        private Thread _contributionPointsManager;
+        private readonly IScheduler _scheduler;
+        private readonly Thread _cleanInactiveUsers;
+        private readonly Thread _contributionPointsManager;
+        private readonly Thread _addBatchesToScheduler;
         private int _intervalInMinutes;
 
         public Automator(int intervalInMinutes, ISchedulerWorkedOnHelper schedulerWorkedOnHelper,
-            IContributionPointCalculator cp)
+            IContributionPointCalculator cp,IScheduler scheduler)
         {
             _intervalInMinutes = intervalInMinutes;
 
             _schedulerWorkedOnHelper = schedulerWorkedOnHelper;
             _cp = cp;
+            _scheduler = scheduler;
 
             _cleanInactiveUsers = new Thread(CleanInactiveUsers);
             _contributionPointsManager = new Thread(ContributionPointsHandler);
+            _addBatchesToScheduler = new Thread(AddBatchesToScheduler);
+            _addBatchesToScheduler.Start();
             _cleanInactiveUsers.Start();
             _contributionPointsManager.Start();
         }
 
         private void CleanInactiveUsers()
         {
-            // Create a timer with a 2 min interval.
+        
             Timer aTimer = new Timer(1000 * 60 * _intervalInMinutes);
             // Hook up the Elapsed event for the timer. 
             aTimer.Elapsed += (s, e) => _schedulerWorkedOnHelper.CleanInactiveUsers();
@@ -37,14 +44,28 @@ namespace WebService.Services
             aTimer.Enabled = true;
         }
 
+        private void AddBatchesToScheduler()
+        {
+            Timer aTimer = new Timer(1000 * 60 * _intervalInMinutes);
+            aTimer.Elapsed += (s, e) => FetchAndAddBatches();
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
         private void ContributionPointsHandler()
         {
-            // Create a timer with a 2 min interval.
+      
             Timer aTimer = new Timer(1000 * 60 * _intervalInMinutes);
             // Hook up the Elapsed event for the timer. 
             aTimer.Elapsed += (s, e) => _cp.CalculateContributionPoints();
             aTimer.AutoReset = true;
             aTimer.Enabled = true;
+        }
+
+        private void FetchAndAddBatches()
+        {
+            List<Batch> batches = new List<Batch>();
+            _scheduler.AddBatches(batches);
+            
         }
     }
 }
