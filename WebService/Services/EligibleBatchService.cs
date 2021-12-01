dcadec8e909fb07c287d96e4265a4fdab81e4eae
soenderby <33911621+soenderby@users.noreply.github.com>
2021-12-01 11:56:59 +0100

    Added EligibleBatchService

---
+++ WebService/Services/EligibleBatchService.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Interfaces;
+using WebService.Models;
+
+namespace WebService.Services
+{
+    public class EligibleBatchService : IEligibleBatchesService
+    {
+        private readonly IDBConnectionFactory _connectionFactory;
+        public EligibleBatchService(IDBConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public IEnumerable<Batch> GetEligibleBatches(int pointLimit)
+        {
+            MySqlConnection conn = _connectionFactory.GetConnection();
+            IEnumerable<Batch> result = conn.Query<Batch>(@"SELECT *
+                                            FROM Batch
+                                            INNER JOIN Users U on Batch.ownedBy = U.username
+                                            WHERE U.points >= @Points", new { Points = pointLimit });
+            return result;
+        }
+    }
+}
