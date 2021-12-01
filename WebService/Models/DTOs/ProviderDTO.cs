72f179e5316c7317cf1f4a5f97c53d0c6f7550f2
dkalaxdk <alax0013@gmail.com>
2021-12-01 14:25:37 +0100

    Updated TaskController.cs to now use Scheduler.

---
+++ WebService/Models/DTOs/ProviderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebService.Models.DTOs
+{
+    public class ProviderDTO
+    {
+        [Required]
+        public string Username;
+
+        public User ToUser()
+        {
+            return new User(Username, null);
+        }
+    }
+}
